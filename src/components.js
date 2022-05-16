5b7426a4e4050eb9d884f6453da14ef9e9bd708d
ValentinGTrapaga <gonzaleztrapagav@gmail.com>
2022-05-16 18:28:25 -0300

    final commit - no media queries - just JS components practice

--- src/components.js
+++ src/components.js
@@ -12,19 +12,108 @@ export function infoDiv(title, info) {
     return infoDiv
 } //Primer componente para mi pagina, para el Home
 
+export function menuItem(title, price, description) {
+    const menuDiv = document.createElement('div')
+    menuDiv.className = "infoText"
+    const menuTitle = document.createElement('h3')
+    menuTitle.className = "menuTitle"
+    menuTitle.innerText = title
+    const itemPrice = document.createElement('h4')
+    itemPrice.className = 'itemPrice'
+    itemPrice.innerText = `$ ${price}`
+    menuDiv.appendChild(menuTitle)
+    menuDiv.appendChild(itemPrice)
+    if (description != undefined) {
+        const itemDesc = document.createElement('p')
+        itemDesc.className = 'descriptionItem'
+        itemDesc.innerText = `Contiene: ${description}`
+        menuDiv.appendChild(itemDesc)
+    }
+    return menuDiv
+}
+
+let menuEntradas = [
+    {
+        title: "Olivas Marinadas",
+        price: 390
+    },
+    {
+        title: "Girgolas fritas, crema de caju y limon",
+        price: 810
+    },
+    {
+        title: "Uvas, stracciatella y pistacho",
+        price: 990
+    },
+    {
+        title: "Faina",
+        price: 210
+    },
+]
+
+let menuPrincipales = [
+    {
+        title: "Marinara",
+        price: 750,
+        description: "Salsa de tomate, ajo y albahaca.",
+    },
+    {
+        title: "Margherita",
+        price: 890,
+        description: "Salsa de tomate, mozzarella fior di latte, albahaca y parmesano.",
+    },
+    {
+        title: "Girgolas",
+        price: 1300,
+        description: "Crema de ajo asado, fiordi latte, girgolas y portobellos.",
+    },
+    {
+        title: "Anchoas",
+        price: 810,
+        description: "Salsa de tomate con anchoas, oregano, ajo, migas de pan y olivas negras.",
+    },
+    {
+        title: "Atte",
+        price: 1300,
+        description: "Salsa de tomate, mozzarella fior de latte, gorgonzola, oregano, parmigiano y provolone.",
+    },
+]
+
 export function home() {
     const mainDiv = document.createElement('div')
-    mainDiv.id = "mainDiv"
-    const titleDiv = document.createElement('h1')
-    titleDiv.innerText = "Le Cepage"
-    titleDiv.className = "title"
-    mainDiv.appendChild(titleDiv)
-    const div1 = infoDiv("Sobre nosotros","simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum")
+    mainDiv.className = "mainDiv"
+    const div1 = infoDiv("Sobre nosotros", "Simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum")
     mainDiv.appendChild(div1)
-    const div2 = infoDiv("Horarios","Lunes a Viernes, de 08:00 a 12:00")
+    const div2 = infoDiv("Horarios", "Viernes a Domingo — 12.30 - 16.30 & 20:00 - 00:00")
     mainDiv.appendChild(div2)
-    const div3 = infoDiv("Direccion","Avenida Siempre Viva 123")
-    mainDiv.appendChild(div3)
+    mainDiv.appendChild(infoDiv("Direccion", "Avenida Siempre Viva 123"))
+    mainDiv.appendChild(infoDiv("Contacto", "011-12345678"))
+    return mainDiv
+}
+
+export function menu() {
+    const mainDiv = document.createElement('div')
+    mainDiv.className = "mainDiv"
+    const entradas = document.createElement('div')
+    entradas.className = "entradas infoDiv"
+    let titleEntradas = document.createElement('h2')
+    titleEntradas.className = "tituloMenu"
+    titleEntradas.innerText = "Entradas"
+    entradas.appendChild(titleEntradas)
+    menuEntradas.map(item => {
+        entradas.appendChild(menuItem(item.title, item.price))
+    })
+    mainDiv.appendChild(entradas)
+    const pizzas = document.createElement('div')
+    pizzas.className = "pizzas infoDiv"
+    let titlePizzas = document.createElement('h2')
+    titlePizzas.className = "tituloMenu"
+    titlePizzas.innerText = "Pizzas"
+    pizzas.appendChild(titlePizzas)
+    menuPrincipales.map(item => {
+        pizzas.appendChild(menuItem(item.title, item.price, item.description))
+    })
+    mainDiv.appendChild(pizzas)
     return mainDiv
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import './styles.css'
-import { home, infoDiv } from './components'
+import './pizza-background.jpg'
+import { home, menu } from './components'
+
+const menuButton = document.querySelector('#menu')
+const homeButton = document.querySelector('#home')
+
+function renderMenu() {
+    app.innerHTML = ""
+    console.log("Rendering menu...")
+    app.appendChild(menu())
+    console.log("Menu rendered")
+}
+
+function renderHome() {
+    app.innerHTML = ""
+    console.log("Rendering home...")
+    app.appendChild(home())
+    console.log("Home rendered")
+}
 
 const app = document.getElementById('app')
 app.appendChild(home())
+menuButton.addEventListener('click', renderMenu)
+homeButton.addEventListener('click', renderHome)
